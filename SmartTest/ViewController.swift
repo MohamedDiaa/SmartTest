179fe5fd923e9713857b0a3d45a273f5240cf198
Mohamed Alwakil <mdalwakil@outlook.com>
2020-10-29 14:25:20 +0100

    Initial Commit

---
+++ SmartTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartTest
+//
+//  Created by Mohamed Diaa on 2020-10-29.
+//  Copyright © 2020 Mohamed Diaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
