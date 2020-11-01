af91d4a385836b17addcf4d5ebc914bf609e1591
Mohamed Alwakil <mdalwakil@outlook.com>
2020-11-01 23:45:37 +0100

    add start button

--- SmartTest/Scenes/SelectGif/SelectGifTableViewController.swift
+++ SmartTest/Scenes/SelectGif/SelectGifTableViewController.swift
@@ -24,27 +24,32 @@ class SelectGifTableViewController: UITableViewController, SelectGifDisplayLogic
         search.obscuresBackgroundDuringPresentation = false
         search.searchBar.placeholder = "Type something here to search"
         navigationItem.searchController = search
-
         
+        let startButton = UIBarButtonItem(title: "Start",
+                                          style: .plain,
+                                          target: self,
+                                          action: #selector(start))
+        
+        navigationItem.rightBarButtonItems = [startButton]
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
     }
     
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
-
+    
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
         // Configure the cell...
         cell.textLabel?.text = "Hello"
         cell.textLabel?.textColor = .black
@@ -52,20 +57,24 @@ class SelectGifTableViewController: UITableViewController, SelectGifDisplayLogic
         return cell
     }
     
-
+    
     
     // MARK: - display logic
     
     func updateSearchResults(for searchController: UISearchController) {
         guard let text = searchController.searchBar.text else { return }
         print(text)
-       }
+    }
     
     func displaySearchForGif(viewModel: SelectGif.SearchForGif.ViewModel) {
         
     }
     
     func displaySelectGif(viewModel: SelectGif.SelectGif.ViewModel){
+        
+    }
+    
+    @IBAction func start() {
         
     }
 }
